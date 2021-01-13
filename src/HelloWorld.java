4cf7d1517e090378849e3da52a0f5b498ae74896
Maciej Szulborski <m.szulborski56@gmail.com>
2021-01-13 20:29:17 +0100

    Added one line of text

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World !");
+        System.out.println("Git Training");
     }
 }
